aa2a1b84e5832a89ff2cc06fb002e2aaf63787e9
Davidk0Fiddler <111727231+Davidk0Fiddler@users.noreply.github.com>
2024-08-28 10:48:00 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById("rolunk").addEventListener("click", function() {
+    const target = document.getElementById("section2");
+    target.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById("blog").addEventListener("click", function() {
+    const target = document.getElementById("section3");
+    target.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById("kapcsolat").addEventListener("click", function() {
+    const target = document.getElementById("section4");
+    target.scrollIntoView({ behavior: 'smooth' });
+});
+
