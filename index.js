f834295312a7ff757247c443887917a80e34b262
Davidk0Fiddler <111727231+Davidk0Fiddler@users.noreply.github.com>
2024-10-17 20:32:39 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ document.getElementById("kapcsolat").addEventListener("click", function() {
     target.scrollIntoView({ behavior: 'smooth' });
 });
 
+let banner = document.getElementById("banner");
+
+let banner_title1 = document.getElementById("banner-title1");
+let banner_title2 = document.getElementById("banner-title2");
+
+banner_title1.addEventListener("mouseover", function(){
+    console.log("a");
+    banner.style.backgroundImage = "url(start.jpg)";
+});
+
+banner_title2.addEventListener("mouseover", function(){
+    console.log("b");
+    banner.style.backgroundImage = "url(nyiltnap.png)";
+});
